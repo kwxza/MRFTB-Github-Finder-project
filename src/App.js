a8a13add80011ed565f304aecfe83312790f3068
Kasaine Kipusi <51935237+kwxza@users.noreply.github.com>
2020-06-04 15:48:42 -0400

    Modified Search.js to pass input text props upwards to App.js through onSubmit function. Modified http query in App.js to make http request to GitHub search endpoint using text props passed from search component.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
         loading: false,
     };
 
-    async componentDidMount() {
+    searchUsers = async (text) => {
         this.setState({ loading: true });
 
         const res = await axios({
-            url: 'https://api.github.com/users',
+            url: `https://api.github.com/search/users?q=${text}`,
             method: 'get',
             auth: {
                 username: `${process.env.REACT_APP_GITHUB_CLIENT_ID}`,
@@ -23,15 +23,15 @@ class App extends Component {
             },
         });
 
-        this.setState({ users: res.data, loading: false });
-    }
+        this.setState({ users: res.data.items, loading: false });
+    };
 
     render() {
         return (
             <div className='App'>
                 <Navbar />
                 <div className='container'>
-                    <Search />
+                    <Search searchUsers={this.searchUsers} />
                     <Users
                         loading={this.state.loading}
                         users={this.state.users}
